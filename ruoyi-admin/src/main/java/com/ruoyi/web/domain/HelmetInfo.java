dd10a857b373033e189ff32cc5e7ce37f0e9a401
majun <mjceoc0896@126.com>
2022-06-30 17:33:33 +0800

    Add HelmetInfo class

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/domain/HelmetInfo.java
@@ -0,0 +1,44 @@
+package com.ruoyi.web.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+/**
+ * 安全帽监测告警图片实体类 helmet_info
+ *
+ * @author maj
+ * @date 2022-05-13
+ */
+@Getter
+@Setter
+@Data
+@TableName(value = "helmet_info")
+public class HelmetInfo {
+
+    /** 图片ID(自增) */
+    private Long id;
+
+    /** 设备编号 */
+    private String deviceId;
+
+    /** 设备名称 */
+    private String deviceName;
+
+    /** 图片名称 */
+    private String imageName;
+
+    /** 图片类型 */
+    private String imageType;
+
+    /** 图片大小 */
+    private Double imageSize;
+
+    /** 图片链接 */
+    private String imageUrl;
+
+    /** 发送时间 */
+    private Date creatTime;
+}
