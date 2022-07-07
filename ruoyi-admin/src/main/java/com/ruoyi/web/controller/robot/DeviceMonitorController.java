aabd3396e177cf9c8b08648a79b93f9ae425a99d
majun <mjceoc0896@126.com>
2022-07-07 10:54:37 +0800

    Add DeviceMonitorController Class

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/robot/DeviceMonitorController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/robot/DeviceMonitorController.java
@@ -1,12 +1,17 @@
 package com.ruoyi.web.controller.robot;
 
+import cn.hutool.core.date.DateTime;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.web.domain.DeviceInfo;
+import com.ruoyi.web.service.IDeviceMonitorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * 电科院仪表监测控制层
@@ -18,6 +23,9 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping(value = "/device/monitor")
 public class DeviceMonitorController extends BaseController {
+
+    @Autowired
+    private IDeviceMonitorService deviceMonitorService;
 
     /**
      *
@@ -31,10 +39,27 @@ public class DeviceMonitorController extends BaseController {
             return AjaxResult.error("上传文件为空!");
         }
 
+        String url = null;
+        try {
+            url = deviceMonitorService.uploadDeviceImage(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        return AjaxResult.success();
+        // 取得机器人上传图片参数
+        DeviceInfo deviceInfo = new DeviceInfo();
+        deviceInfo.setDeviceId(request.getParameter("deviceId"));
+        deviceInfo.setDeviceName(request.getParameter("deviceName"));
+        deviceInfo.setDeviceType(request.getParameter("deviceType"));
+        deviceInfo.setImageName(request.getParameter("imageName"));
+        deviceInfo.setImageType(request.getParameter("imageType"));
+        deviceInfo.setImageSize(Double.valueOf(request.getParameter("imageSize")));
+        deviceInfo.setCreatTime(DateTime.of(request.getParameter("creatTime"),"yyyy-MM-dd HH:mm:ss"));
 
+        // 保存仪表监测图片
+        deviceMonitorService.saveDeviceImage(url,deviceInfo);
 
+        return AjaxResult.success("仪表监测图片上传成功!");
     }
 
 
