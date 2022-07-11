485048ff79e3c6d80c1313a498c23d8a078ce665
majun <mjceoc0896@126.com>
2022-07-11 17:16:10 +0800

    Add selectDeviceList Interface Method

--- ruoyi-admin/src/main/java/com/ruoyi/web/service/IDeviceMonitorService.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/service/IDeviceMonitorService.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.jws.WebService;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 仪表监测图片 服务层
@@ -16,17 +17,24 @@ import java.io.IOException;
 public interface IDeviceMonitorService {
 
     /**
-     *
-     * @param file
+     * 上传仪表监测图片(file类型)
+     * @param file 上传文件
      * @return
      * @throws IOException
      */
     String uploadDeviceImage(MultipartFile file) throws IOException;
 
     /**
-     *
-     * @param url
-     * @param deviceInfo
+     * 保存仪表监测图片(file类型)
+     * @param url 图片链接
+     * @param deviceInfo 设备信息
      */
     void saveDeviceImage(String url, DeviceInfo deviceInfo);
+
+    /**
+     * 查询仪表信息列表
+     * @param deviceInfo 设备信息
+     * @return 设备信息列表
+     */
+    List<DeviceInfo> selectDeviceList(DeviceInfo deviceInfo);
 }
