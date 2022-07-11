eaf288b189e1f545a21a092fe3665ee61e9538aa
majun <mjceoc0896@126.com>
2022-07-11 17:18:15 +0800

    Add list method

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/robot/DeviceMonitorController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/robot/DeviceMonitorController.java
@@ -4,14 +4,17 @@ import cn.hutool.core.date.DateTime;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.web.domain.DeviceInfo;
 import com.ruoyi.web.service.IDeviceMonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 电科院仪表监测控制层
@@ -57,7 +60,7 @@ public class DeviceMonitorController extends BaseController {
         deviceInfo.setImageType(request.getParameter("imageType"));
         deviceInfo.setImageSize(Double.valueOf(request.getParameter("imageSize")));
         deviceInfo.setImageResult(Double.valueOf(request.getParameter("imageResult")));
-        deviceInfo.setCreatTime(DateTime.of(request.getParameter("creatTime"),"yyyy-MM-dd HH:mm:ss"));
+        deviceInfo.setCreateTime(DateTime.of(request.getParameter("creatTime"),"yyyy-MM-dd HH:mm:ss"));
 
         // 保存仪表监测图片
         deviceMonitorService.saveDeviceImage(url,deviceInfo);
@@ -65,5 +68,16 @@ public class DeviceMonitorController extends BaseController {
         return AjaxResult.success("仪表监测图片上传成功!");
     }
 
-
+    /**
+     * 查询仪表信息列表
+     * @param deviceInfo 设备信息
+     * @return 设备信息列表
+     */
+    @PreAuthorize("@ss.hasPermi('device:monitor:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(DeviceInfo deviceInfo) {
+        startPage();
+        List<DeviceInfo> list = deviceMonitorService.selectDeviceList(deviceInfo);
+        return getDataTable(list);
+    }
 }
