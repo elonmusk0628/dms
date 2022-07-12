865d9e425948bb05eaf4ced727e9ca2678ae07ad
majun <mjceoc0896@126.com>
2022-07-12 14:10:05 +0800

    Add DeviceMonitor Vue

---
+++ ruoyi-ui/src/views/robot/device/index.vue
@@ -0,0 +1,133 @@
+<template>
+	<div class="app-container">
+		<el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch">
+			<el-form-item label="设备名称" prop="deviceName">
+			  <el-input
+			    v-model="queryParams.deviceName"
+			    placeholder="请输入设备名称"
+			    clearable
+			    style="width: 240px"
+			    @keyup.enter.native="handleQuery"
+			  />
+			</el-form-item>
+			<el-form-item label="设备类型" prop="deviceType">
+			  <el-select
+			    v-model="queryParams.deviceType"
+			    placeholder="设备类型"
+			    clearable
+			    style="width: 240px"
+			  >
+			    <el-option
+			      v-for="dict in dict.type.device_type"
+			      :key="dict.value"
+			      :label="dict.label"
+			      :value="dict.value"
+			    />
+			  </el-select>
+			</el-form-item>
+			<el-form-item label="图片名称" prop="imageName">
+        <el-input
+          v-model="queryParams.imageName"
+          placeholder="请输入图片名称"
+          clearable
+          style="width: 240px;"
+          @keyup.enter.native="handleQuery"
+        />
+			</el-form-item>
+      <el-form-item label="图片类型" prop="imageType">
+        <el-select
+          v-model="queryParams.imageType"
+          placeholder="图片类型"
+          clearable
+          style="width: 240px"
+        >
+          <el-option
+            v-for="dict in dict.type.image_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+
+			<el-form-item>
+        		<el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        		<el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+
+			<el-table v-loading="loading" :data="deviceList" >
+				<el-table-column label="设备编号" width="95"  align="center" prop="deviceId" />
+				<el-table-column label="设备名称" width="80"  align="center" prop="deviceName"/>
+				<el-table-column label="设备类型" width="140" align="center" prop="deviceType"/>
+        <el-table-column label="图片名称" width="125" align="center" prop="imageName"/>
+        <el-table-column label="图片类型" width="80"  align="center" prop="imageType"/>
+        <el-table-column label="图片大小" width="80"  align="center" prop="imageSize"/>
+        <el-table-column label="仪表数值" width="80" align="center" prop="imageResult"/>
+        <el-table-column label="图片链接" width="750" align="center" prop="imageUrl"/>
+        <el-table-column label="创建时间" width="160" align="center" prop="createTime" >
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+		</el-form>
+	</div>
+</template>
+
+<script>
+  import {listDevice} from "@/api/robot/device";
+
+export default {
+	name: "device",
+  dicts: ['device_type', 'image_type'],
+	data() {
+		return {
+      // 遮罩层
+      loading: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 菜单表格树数据
+      deviceList: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        deviceName: undefined,
+        deviceType: undefined,
+        imageName: undefined,
+        imageType: undefined
+      }
+    }
+	},
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    /** 查询仪表信息列表 */
+    getList() {
+      this.loading = true;
+      listDevice(this.queryParams).then(response => {
+        this.deviceList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    }
+
+  }
+}
+</script>
+
+<style>
+</style>
