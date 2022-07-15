65e77cab0a310b0a27dd1254146d9a3b1178c699
majun <mjceoc0896@126.com>
2022-07-15 17:19:20 +0800

    Add selectHelmetList interface

--- ruoyi-admin/src/main/java/com/ruoyi/web/service/IUploadHelmetImageService.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/service/IUploadHelmetImageService.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.jws.WebService;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 安全帽监测图片 服务层
@@ -17,19 +18,24 @@ public interface IUploadHelmetImageService {
 
     /**
      * 上传安全帽监测图片(file类型)
-     * @param file
-     * @return
+     * @param file 图片文件
+     * @return 图片上传结果
      */
     String uploadHelmetImage(MultipartFile file) throws IOException;
 
 
     /**
      * 保存安全帽监测图片(file类型)
-     * @param url
-     * @param helmetInfo
+     * @param url 图片链接
+     * @param helmetInfo 安全帽信息实体类
      */
     void saveHelmetImage(String url, HelmetInfo helmetInfo);
 
 
-
+    /**
+     * 查询安全帽信息列表
+     * @param helmetInfo 安全帽信息实体类
+     * @return 安全帽监测信息列表
+     */
+    List<HelmetInfo> selectHelmetList(HelmetInfo helmetInfo);
 }
