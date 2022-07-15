b5452b5e0292b836ba1f9bef9afbff62125f77da
majun <mjceoc0896@126.com>
2022-07-15 17:18:37 +0800

    Add selectHelmetList

--- ruoyi-admin/src/main/java/com/ruoyi/web/mapper/UploadHelmetImageMapper.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/mapper/UploadHelmetImageMapper.java
@@ -3,6 +3,8 @@ package com.ruoyi.web.mapper;
 import com.ruoyi.web.domain.HelmetInfo;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * 上传安全帽监测图片 数据层
  *
@@ -18,6 +20,13 @@ public interface UploadHelmetImageMapper {
      * @return int
      */
     int saveHelmetImage(HelmetInfo helmetInfo);
+
+    /**
+     * 查询安全帽信息列表
+     * @param helmetInfo 安全帽信息实体类
+     * @return 安全帽监测信息列表
+     */
+    List<HelmetInfo> selectHelmetList(HelmetInfo helmetInfo);
 
 
 }
