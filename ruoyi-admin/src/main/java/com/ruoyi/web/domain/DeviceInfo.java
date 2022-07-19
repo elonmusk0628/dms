10f2606698dd2f15abd2cb957c4ff21fbadde4f5
majun <mjceoc0896@126.com>
2022-07-19 17:57:06 +0800

    Modify DeviceInfo

--- ruoyi-admin/src/main/java/com/ruoyi/web/domain/DeviceInfo.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/domain/DeviceInfo.java
@@ -2,6 +2,8 @@ package com.ruoyi.web.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,36 +20,47 @@ import java.util.Date;
 @Setter
 @Data
 @TableName(value = "device_info")
-public class DeviceInfo {
+public class DeviceInfo extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
 
     /** 图片ID(自增) */
     private Long id;
 
     /** 设备编号 */
+    @Excel(name = "设备编号", width = 15)
     private String deviceId;
 
     /** 设备名称 */
+    @Excel(name = "设备名称", width = 15)
     private String deviceName;
 
     /** 设备类型 */
+    @Excel(name = "设备类型", width = 15)
     private String deviceType;
 
     /** 图片名称 */
+    @Excel(name = "图片名称", width = 15)
     private String imageName;
 
     /** 图片类型 */
+    @Excel(name = "图片类型", width = 15)
     private String imageType;
 
     /** 图片大小 */
+    @Excel(name = "图片大小", width = 15, cellType = Excel.ColumnType.NUMERIC)
     private Double imageSize;
 
     /** 识别结果 */
+    @Excel(name = "识别结果", width = 15)
     private String imageResult;
 
     /** 图片链接 */
+    @Excel(name = "仪表图片", width = 17, cellType = Excel.ColumnType.IMAGE, height = 100)
     private String imageUrl;
 
     /** 发送时间 */
+    @Excel(name = "发送时间", width = 17, dateFormat = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 }
