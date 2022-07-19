d4ee02293ef70e10a429f89be386975f32113658
majun <mjceoc0896@126.com>
2022-07-19 17:56:31 +0800

    Add export

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/warning/UploadHelmetImageController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/warning/UploadHelmetImageController.java
@@ -1,10 +1,13 @@
 package com.ruoyi.web.controller.warning;
 
 import cn.hutool.core.date.DateTime;
+import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.web.domain.HelmetInfo;
 import com.ruoyi.web.service.IUploadHelmetImageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -76,5 +80,19 @@ public class UploadHelmetImageController extends BaseController {
         startPage();
         List<HelmetInfo> list = uploadHelmetImageService.selectHelmetList(helmetInfo);
         return getDataTable(list);
+    }
+
+    /**
+     * 导出安全帽监测信息列表
+     * @param response 前端请求响应
+     * @param helmetInfo 安全帽信息实体类
+     */
+    @Log(title="安全帽监测", businessType=BusinessType.EXPORT)
+    @PreAuthorize("@ss.hasPermi('robot:helmet:export')")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, HelmetInfo helmetInfo) {
+        List<HelmetInfo> list = uploadHelmetImageService.selectHelmetList(helmetInfo);
+        ExcelUtil<HelmetInfo> util = new ExcelUtil<>(HelmetInfo.class);
+        util.exportExcel(response,list,"安全帽监测数据");
     }
 }
